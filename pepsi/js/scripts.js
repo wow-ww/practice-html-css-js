cb6c8f171cb9500e381b99c847674813ad731d96
wow-ww <WuShiruo@outlook.com>
2022-05-29 22:03:13 +0800

    pepsi提交

---
+++ pepsi/js/scripts.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+    const src = document.querySelector('.sec');
+    src.style.background = color;
+}
