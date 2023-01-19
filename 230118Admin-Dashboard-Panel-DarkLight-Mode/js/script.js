e5a44999bfdd0d5adc2074e85a868f720e6f38fa
wow-ww <Wushiruo@outlook.com>
2023-01-19 18:06:34 +0800

    admin dashboard basically finished

--- 230118Admin-Dashboard-Panel-DarkLight-Mode/js/script.js
+++ 230118Admin-Dashboard-Panel-DarkLight-Mode/js/script.js
@@ -1,6 +1,22 @@
 const body = document.querySelector("body");
 const modeToggle = body.querySelector(".mode-toggle");
+const sidebar= body.querySelector("nav");
+const sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
 
 modeToggle.addEventListener("click", () => {
   body.classList.toggle("dark");
+  if(body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  }else {
+    localStorage.setItem("mode", "light");
+  }
+})
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
 })
